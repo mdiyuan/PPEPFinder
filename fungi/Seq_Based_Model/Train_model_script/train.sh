d367da91438633391644827903d64f3d882f9aad
mdiyuan <111565934+mdiyuan@users.noreply.github.com>
2025-07-09 10:56:11 +0800

    Create train.sh

---
+++ fungi/Seq_Based_Model/Train_model_script/train.sh
@@ -0,0 +1,19 @@
+for i in {0..4}
+do
+   python train.py \
+   		--model effectortransformer \
+		--data_dir ./seqs \
+		--batch_size 4 \
+		--lr 5e-5 \
+		--weight_decay 4e-5 \
+		--dropout_rate 0.4 \
+		--num_layers 1 \
+		--num_heads 4 \
+		--warm_epochs 1 \
+		--patience 5 \
+		--lr_scheduler cosine \
+		--lr_decay_steps 30 \
+		--kfold 5 \
+		--fold_num $i \
+		--log_dir train_models
+done
